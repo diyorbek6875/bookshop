dcb6fd0fd9ab45cb3dca5718e173685b6f378420
diyorbek6875 <diyorbekabdusalomov32@gmail.com>
2023-11-09 09:41:29 +0500

    bookshop models

--- API/admin.py
+++ API/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Customer, Contact
+admin.site.register(Customer)
+admin.site.register(Contact)
--- API/models.py
+++ API/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    username = models.CharField(max_length=64, unique=True)
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    def __str__(self) -> str:
+        return self.full_name
+    
+
+
+class Contact(models.Model):
+    customer = models.OneToOneField(Customer,on_delete=models.CASCADE)
+    email= models.EmailField()
+    address = models.CharField(max_length=512)
+    phone = models.CharField(max_length=13)
+
+
+    def __str__(self) -> str:
+        return self.full_name
